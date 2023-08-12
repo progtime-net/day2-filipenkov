6f8b8ec88ba7c409c8d1407e1408d680715bde95
Ivan Filipenkov <hello@vanutp.dev>
2023-08-12 12:07:40 +0300

    init

---
+++ day2_middle/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace day2_middle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
