dd04d31d1404fac1fba904fd1175a6e050366e7c
Ivan Filipenkov <hello@vanutp.dev>
2023-08-12 12:25:44 +0300

    add greetings

--- day2_middle/Program.cs
+++ day2_middle/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("What's your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}");
         }
     }
 }
